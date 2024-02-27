642292cbcbd484d6600ef048a99982069c89ab6d
Pavel Bortnovskyi <113463594+PavelBortnovskyi@users.noreply.github.com>
2024-02-27 12:25:51 +0000

    Task 2 Query added

---
+++ Scripts/Query 2.sql
@@ -0,0 +1,10 @@
+#LEFT JOIN to retrieve salesman with zero sales
+
+SELECT emp.firstName,
+       emp.lastName,
+       emp.title,
+       emp.startDate,
+       sales.salesId 
+FROM employee emp
+LEFT JOIN sales ON emp.employeeId = sales.employeeId
+WHERE emp.title = 'Sales Person' AND sales.salesId IS NULL
